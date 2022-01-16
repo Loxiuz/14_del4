9425e0bda49fd3b67296d7ea24366000988965c9
malte <maltebjoerklund@gmail.com>
2022-01-16 18:55:11 +0100

    små ændringer i ChanceDeck

--- src/game/ChanceDeck.java
+++ src/game/ChanceDeck.java
@@ -2,7 +2,7 @@ package game;
 
 public class ChanceDeck {
 
-    int randomNr = (int) (Math.random() * 24 + 1);
+    int randomNr = (int) (Math.random() * 31 + 1);
     GameBoard gameBoard = new GameBoard();
     int field = 0;
 
@@ -11,42 +11,46 @@ public class ChanceDeck {
         int[] amount = new int[23];
 
         //Betal:
-        amount[2] = 1000;
-        amount[3] = 300;
-        amount[4] = 200;
-        amount[5] = 3000;
+        amount[0] = 1000;
+        amount[1] = 300;
+        amount[2] = 200;
+        amount[3] = 3000;
+        amount[4] = 1000;
+        amount[5] = 200;
         amount[6] = 1000;
         amount[7] = 200;
-        amount[8] = 1000;
-        amount[9] = 200;
-        amount[10] = 2000;
+        amount[8] = 2000;
         //Modtag:
-        amount[11] = 500;
+        amount[9] = 500;
+        amount[10] = 1000;
+        amount[11] = 3000;
         amount[12] = 1000;
-        amount[13] = 3000;
+        amount[13] = 1000;
         amount[14] = 1000;
         amount[15] = 1000;
-        amount[16] = 1000;
-        amount[17] = 1000;
+        amount[16] = 200;
+        amount[17] = 40000;
         amount[18] = 200;
-        amount[19] = 40000;
-        amount[20] = 200;
-        amount[21] = 500;
-        amount[22] = 500;
+        amount[19] = 500;
+        amount[20] = 500;
 
-        if (card <= 10) {
+        if (card <= 8) {
             player.getAccount().withdraw(amount[card]);
         }
-        else if (card < 23) {
+        else if (card <= 20) {
             player.getAccount().deposit(amount[card]);
         }
         else{
             switch (card) {
+                case 21: case21(player);
+                    break;
+                case 22: case22(player);
+                    break;
                 case 23: case23(player);
                     break;
-                case 24: case24(player);
+                case 24: case24();
                     break;
-                case 25: case25(player);
+                case 25: case25();
                     break;
                 case 26: case26();
                     break;
@@ -56,13 +60,9 @@ public class ChanceDeck {
                     break;
                 case 29: case29();
                     break;
-                case 30: case30();
+                case 30: case30(player);
                     break;
-                case 31: case31();
-                    break;
-                case 32: case32(player);
-                    break;
-                case 33: case33(player);
+                case 31: case31(player);
                     break;
             }
             if (field > 0){
@@ -71,36 +71,35 @@ public class ChanceDeck {
             }
         }
     }
-    private void case23(Player player){ // Player goes to jail.
+
+    private void case21(Player player){ // Player goes to jail.
         player.setPosition(10);
         player.getJailed();
     }
-    private void case24(Player player){ // Player recieves GOJC.
+    private void case22(Player player){ // Player recieves GOJC.
         player.setGOJC(1 + player.getGOJC());
     }
-    private void case25(Player player){ // Ryk frem til start
+    private void case23(Player player){ // Ryk frem til start
         player.movePlayer(40 - player.getPosition());
     }
-    private void case26(){field = 12;} // Ryk frem til Frederiksberg Allé
+    private void case24(){field = 12;} // Ryk frem til Frederiksberg Allé
 
-    private void case27(){field = 16;} // Tag med Mols-Linien
+    private void case25(){field = 16;} // Tag med Mols-Linien
 
-    private void case28(){field = 20;} //Ryk frem til Strandvejen
+    private void case26(){field = 20;} //Ryk frem til Strandvejen
 
-    private void case29(){field = 25;} // Ryk frem til Grønningen
+    private void case27(){field = 25;} // Ryk frem til Grønningen
 
-    private void case30(){field = 33;} // Ryk frem til Vimmelskaftet
+    private void case28(){field = 33;} // Ryk frem til Vimmelskaftet
 
-    private void case31(){field = 40;} // Tag til Rådhuspladsen
+    private void case29(){field = 40;} // Tag til Rådhuspladsen
 
-    private void case32(Player player){ // Ryk 3 felter frem
+    private void case30(Player player){ // Ryk 3 felter frem
         player.movePlayer(3);
     }
-    private void case33(Player player){ // Ryk 3 felter tilbage
+    private void case31(Player player){ // Ryk 3 felter tilbage
         player.movePlayer(-3);
     }
-
-
 
     public void drawRandomCard(Player player){
         chanceDeck(player, randomNr-1);
